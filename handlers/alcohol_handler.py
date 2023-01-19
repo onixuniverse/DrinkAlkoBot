855f8bde90cae0a8e02fd9a5aa2abb2bd442731e
onixuniverse <warofmyhome@gmail.com>
2023-01-19 18:14:23 +0500

    add alcohol handler

---
+++ handlers/alcohol_handler.py
@@ -0,0 +1,107 @@
+import re
+
+from aiogram import Dispatcher, types
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.types import ReplyKeyboardRemove
+
+from db import db
+from keyboards import alcohol, general
+
+
+class FSMAddDrink(StatesGroup):
+    drink = State()
+    date = State()
+    count = State()
+    price = State()
+
+
+async def alcohol_start(message: types.Message):
+    await FSMAddDrink.drink.set()
+
+    await message.answer("🍸 Итак, какой напиток Вы выпили?", reply_markup=alcohol.drinks_keyboard)
+
+
+async def cancel_alcohol_fsm(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+    await state.finish()
+    await message.answer("❌ Добавление напитка отменено.", reply_markup=general.general_keyboard)
+
+
+async def load_drink(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['drink'] = message.text
+
+    await FSMAddDrink.next()
+
+    await message.reply("📅 Когда Вы его выпили?\nНажмите на кнопку или введите дату в формате ДЕНЬ.МЕСЯЦ.ГОД\n\n"
+                        "При вводе некорректной даты, статистика может быть также некорректной.",
+                        reply_markup=alcohol.dates_keyboard)
+
+
+async def load_date(message: types.Message, state: FSMContext):
+    flag = False
+    while not flag:
+        if re.match(r"(0?[1-9]|[12][0-9]|3[01]).(0?[1-9]|1[012]).(2023)", message.text):
+            async with state.proxy() as data:
+                data['date'] = message.text
+            flag = True
+            await FSMAddDrink.next()
+
+            await message.reply("🍷 Введите массу напитка в миллилитрах.", reply_markup=alcohol.count_keyboard)
+        else:
+            await message.reply("Введена некорректная дата!", reply_markup=alcohol.count_keyboard)
+            flag = False
+
+
+async def load_count(message: types.Message, state: FSMContext):
+    flag = False
+    while not flag:
+        if message.text.isdigit():
+            async with state.proxy() as data:
+                data['count'] = message.text
+            flag = True
+            await FSMAddDrink.next()
+
+            await message.reply("💸 Какая стоимость у напитка?", reply_markup=ReplyKeyboardRemove())
+        else:
+            await message.reply("Введена некорректная масса!", reply_markup=ReplyKeyboardRemove())
+            flag = False
+
+
+async def load_price(message: types.Message, state: FSMContext):
+    flag = False
+    while not flag:
+        try:
+            message.text = message.text.replace(",", ".")
+            float(message.text)
+
+            async with state.proxy() as data:
+                data['price'] = message.text
+            flag = True
+
+            await db.add_record(str(message.from_id), state)
+
+            async with state.proxy() as data:
+                print(str(data))
+                await message.answer(f"✅ Отлично, напиток добавлен!\n\nНапиток: {data['drink']}\nДата: {data['date']}\n"
+                                     f"Количество: {data['count']}мл\nЦена: {data['price']}",
+                                     reply_markup=general.general_keyboard)
+
+            await state.finish()
+        except ValueError:
+            await message.reply("Введена некорректная стоимость!", reply_markup=ReplyKeyboardRemove())
+            flag = False
+
+
+def register_handler(dp: Dispatcher):
+    dp.register_message_handler(alcohol_start, regexp="🍸 Добавить напиток", state=None)
+    dp.register_message_handler(cancel_alcohol_fsm, regexp="отмена", state="*")
+    dp.register_message_handler(cancel_alcohol_fsm, Text(equals="отмена", ignore_case=True), state="*")
+    dp.register_message_handler(load_drink, state=FSMAddDrink.drink)
+    dp.register_message_handler(load_date, state=FSMAddDrink.date)
+    dp.register_message_handler(load_count, state=FSMAddDrink.count)
+    dp.register_message_handler(load_price, state=FSMAddDrink.price)
